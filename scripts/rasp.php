7eb93e6993ca66be61fe2039e08689e7acba8352
Marc Urben <aegnor@mittelerde.ch>
2012-11-04 21:13:42 +0100

    james rasp commands are now: ledTest, status, switchOn, switchOff

--- scripts/rasp.php
+++ scripts/rasp.php
@@ -6,36 +6,69 @@ include('/opt/james/settings/settings.php');
 include('/opt/james/include/func.base.php');
 include('/opt/james/include/func.rasp.php');
 
-#init
-#initLoop();
-echo "test ok";
-switchOn(0);
+switch ($GLOBALS['argv'][1]) {
+	case "ledTest":
+		$leds = $GLOBALS['LEDS'];
+		$buttons = $GLOBALS['BUTTONS'];
+		asort($leds);
+		asort($buttons);
 
-/*
-#main loop
-echo "looping...\n";
-$run = true;
-while ($run) {
-	$atHomeCheckLoop++;
-
-	for ($i=4; $i < 8; $i++) {
-		if (buttonCheck($i)) {
-			echo "here we go for button " . $i . "\n";
-			alert("Button " . $i . " pressed.");
-			$GLOBALS['POWERLEDBLINKNUM']++;
+		echo "initializing leds: ";
+		foreach ($leds as $i) {
+			$oldState = wiringpi::digitalRead($i);
+			echo $i . " ";
+			blink($i, 2, 50000);
+			if ($oldState) switchOn($i);
 		}
-	}
-	
-	if ($atHomeCheckLoop > $ATHOMECHECKLOOPS) {
-		$atHomeCheckLoop = 0;
-		blink(3, 1);
-		$return = newEvent("is_at_home");
-		if ($return == 0) {
-			switchOn(3);
-		}
-	}
+		echo "done\n";
 
-	$run = sleepLoop();
+	break;;
+
+	case "status":
+		echo "RaspBerryPi Status Output:\n";
+		echo "_LED_\n";
+		asort($GLOBALS['LEDS']);
+		foreach ($GLOBALS['LEDS'] as $led) {
+			if (wiringpi::digitalRead($led) == 1) {
+				$str = "on";
+			} else {
+				$str = "off";
+			}
+			echo $led . ": " .$str . "\n";
+		}
+
+		echo "_Button_\n";
+		asort($GLOBALS['BUTTONS']);
+		foreach ($GLOBALS['BUTTONS'] as $button) {
+			if (wiringpi::digitalRead($button) == 1) {
+				$str = "high";
+			} else {
+				$str = "low";
+			}
+
+			echo $button . ": " . $str . "\n";
+		}
+	break;;
+
+	case "switchOn":
+		if ($GLOBALS['argv'][2]) {
+			switchOn($GLOBALS['argv'][2]);
+			echo "done";
+		} else {
+			echo "please specify pin";
+		}
+	break;;
+
+	case "switchOff":
+		if ($GLOBALS['argv'][2]) {
+			switchOff($GLOBALS['argv'][2]);
+			echo "done";
+		} else {
+			echo "please specify pin";
+		}
+	break;;
+
+	default:
+		echo "commands: ledTest, status, switchOn, switchOff";
 }
-*/
 ?>
