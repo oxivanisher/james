c2582eb1906d374b708f6d8f0f6f45ff6262ec94
Marc Urben <aegnor@mittelerde.ch>
2012-12-03 20:58:40 +0100

    majoir changes in alerting ... hoping it still works

--- daemon.alert.sh
+++ daemon.alert.sh
@@ -10,23 +10,30 @@ do
 	then
 		if [ $($BASEDIR/new_event.sh is_at_home) -eq 1 ];
 		then
+			mpc -h mpd -q volume 100
+			MPC=$(/root/scripts/mpc_radio_on.sh)
+			sleep 2
+	        mpc -h mpd -q volume -50
+			sleep 1
+			$($BASEDIR/scripts/alert.sh "Welcome! It is now $(date +%H:%M).")
+
 			LOGDATA=$(cat $ALERTCACHE) 
-			rm $ALERTCACHE 
- 
+			rm $ALERTCACHE
+
 			if [ $(echo "$LOGDATA" | wc -l) == 1 ]; 
 			then 
-				$BASEDIR/new_event.sh alert "Nothing happend while we where appart." & 
+				$($BASEDIR/scripts/alert.sh "Nothing happend while we where appart.")
 			else 
-				$BASEDIR/new_event.sh alert "While we where appart, the following things happend:" & 
+				$($BASEDIR/scripts/alert.sh "While we where appart, the following things happend:")
 				echo -e "$LOGDATA" | while read LOGLINE; 
 				do 
-					$BASEDIR/new_event.sh alert "$LOGLINE" & 
+					$($BASEDIR/scripts/alert.sh "${LOGLINE}")
 				done
 
-				$BASEDIR/new_event.sh alert "End of Log." & 
+				$($BASEDIR/scripts/alert.sh "End of Log.") 
 			fi 
  
-			MPC=$(/root/scripts/mpc_radio_on.sh)
+	        mpc -h mpd -q volume +50
 		fi
 	fi
 
@@ -37,19 +44,19 @@ do
         cp $ALERTMESSAGES $ALERTMESSAGES.tmp
         rm $ALERTMESSAGES
         rm $ALERTMESSAGES.lock
-        mpc -h mpd -q volume -30
+        mpc -h mpd -q volume -50
         while read LINE;
         do
             if [ "a$LINE" != "a" ];
             then
 				set -- $LINE
-                echo -e "\tmsg: $LINE ; $1 ; $2"
+                echo -e "\tmsg: $LINE"
 				$($BASEDIR/scripts/alert.sh "${LINE}")
                 sleep 0.5
             fi
         done < $ALERTMESSAGES.tmp
         rm $ALERTMESSAGES.tmp
-        mpc -h mpd -q volume +30
+        mpc -h mpd -q volume +50
     else
         sleep 1
     fi
